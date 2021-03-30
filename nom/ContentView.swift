37cbbabb2ff1973bd8a42d8c4d9408142e9f332c
tomadowley <tdowley@live.co.uk>
2021-03-30 19:18:43 +0100

    Initial Commit

---
+++ nom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nom
+//
+//  Created by Tom Dowley on 30/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nom/nomApp.swift
@@ -0,0 +1,17 @@
+//
+//  nomApp.swift
+//  nom
+//
+//  Created by Tom Dowley on 30/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct nomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
